a89ddb95ea954b6486ce40abdb43d2b9f7b829b8
12liyab <ktliyab7@gmail.com>
2025-07-05 01:52:11 +0000

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,15 @@
 import { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import useScrollToTop from '@/hooks/useScrollToTop';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  useScrollToTop();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
